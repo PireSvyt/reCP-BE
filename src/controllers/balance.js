3726fcfb4eaad10dd62211bd2964115d3660f395
PireSvyt <81512700+PireSvyt@users.noreply.github.com>
2022-04-01 18:05:06 +0200

    changed to user

--- src/controllers/balance.js
+++ src/controllers/balance.js
@@ -3,37 +3,27 @@ const Transaction = require("../models/Transaction");
 exports.computeBalance = (req, res, next) => {
   Transaction.find()
     .then((transactions) => {
-      var users = [{ Alice: 0 }, { Pierre: 0 }];
+      var users = { Alice: 0, Pierre: 0 };
       var factor = 0;
       var share = 0;
-      var by = "";
       transactions
         .forEach((transaction) => {
-          users
-            .forEach((user) => {
-              by = user.keys()[0];
-              if (by === transaction.by) {
-                factor = 1;
-                share = (transaction.for.length - 1) / transaction.for.length;
-              } else {
-                factor = -1;
-                share = 1 / transaction.for.length;
-              }
-              users[by] = users[by] + factor * share * transaction.amount;
-            })
-            .catch((error) =>
-              res
-                .status(400)
-                .json({ message: "problème de boucle dans les users", error })
-            );
+          for (const user of users.keys()) {
+            if (user === transaction.by) {
+              factor = 1;
+              share = (transaction.for.length - 1) / transaction.for.length;
+            } else {
+              factor = -1;
+              share = 1 / transaction.for.length;
+            }
+            users[user] = users[user] + factor * share * transaction.amount;
+          }
         })
         .catch((error) =>
-          res
-            .status(400)
-            .json({
-              message: "problème de boucle dans les transactions",
-              error
-            })
+          res.status(400).json({
+            message: "problème de boucle dans les transactions",
+            error
+          })
         );
       res.status(200).json(users);
     })
