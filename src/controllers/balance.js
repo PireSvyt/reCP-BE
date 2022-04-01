a5593f3382581431a126818f59dd404f86e1d845
PireSvyt <81512700+PireSvyt@users.noreply.github.com>
2022-04-01 18:08:40 +0200

    with log

--- src/controllers/balance.js
+++ src/controllers/balance.js
@@ -1,14 +1,18 @@
 const Transaction = require("../models/Transaction");
 
 exports.computeBalance = (req, res, next) => {
+  var log = [];
   Transaction.find()
     .then((transactions) => {
       var users = { Alice: 0, Pierre: 0 };
       var factor = 0;
       var share = 0;
+      log.push("decaled variables");
       transactions
         .forEach((transaction) => {
+          log.push("loopîng through transactions");
           for (const user of users.keys()) {
+            log.push("loopîng through users");
             if (user === transaction.by) {
               factor = 1;
               share = (transaction.for.length - 1) / transaction.for.length;
@@ -22,12 +26,14 @@ exports.computeBalance = (req, res, next) => {
         .catch((error) =>
           res.status(400).json({
             message: "problème de boucle dans les transactions",
+            log,
             error
           })
         );
+      log.push("sending res");
       res.status(200).json(users);
     })
     .catch((error) =>
-      res.status(400).json({ message: "problème de recherche", error })
+      res.status(400).json({ message: "problème de recherche", log, error })
     );
 };
