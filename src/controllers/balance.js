f9dd711576bbaab79c9511a15270250fe9e98e68
PireSvyt <81512700+PireSvyt@users.noreply.github.com>
2022-04-01 18:38:03 +0200

    no more logs

--- src/controllers/balance.js
+++ src/controllers/balance.js
@@ -1,20 +1,15 @@
 const Transaction = require("../models/Transaction");
 
 exports.computeBalance = (req, res, next) => {
-  var log = [];
   Transaction.find()
     .then((transactions) => {
       var users = { Alice: 0, Pierre: 0 };
       var factor = 0;
       var share = 0;
-      log.push("loopîng through transactions");
       var jsonTransaction = {};
       transactions.forEach((transaction) => {
-        log.push("transaction");
         jsonTransaction = transaction.toObject();
-        log.push(jsonTransaction);
         for (var [user, count] of Object.entries(users)) {
-          log.push("user " + user);
           if (user === jsonTransaction.by) {
             factor = 1;
             share =
@@ -24,14 +19,11 @@ exports.computeBalance = (req, res, next) => {
             share = 1 / jsonTransaction.for.length;
           }
           users[user] = users[user] + factor * share * jsonTransaction.amount;
-          log.push(factor * share * jsonTransaction.amount);
         }
-        log.push(users);
       });
-      log.push("sending res");
       res.status(200).json(users);
     })
     .catch((error) =>
-      res.status(400).json({ message: "problème de recherche", log, error })
+      res.status(400).json({ message: "problème de recherche", error })
     );
 };
