8aa2e8755721e075497895f7ccac15616b4f77ee
PireSvyt <81512700+PireSvyt@users.noreply.github.com>
2022-04-01 17:34:34 +0200

    changing api to balance compute

--- src/routes/balance.js
+++ src/routes/balance.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const balanceCtrl = require("../controllers/balance");
 
-router.get("/", balanceCtrl.computeBalance);
+router.get("/compute", balanceCtrl.computeBalance);
 
 module.exports = router;
