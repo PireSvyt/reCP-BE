73d0e9e0dcb0aef4d16aa48387a9c91906df9fc2
PireSvyt <81512700+PireSvyt@users.noreply.github.com>
2022-04-01 17:48:57 +0200

    capturing error

--- src/controllers/balance.js
+++ src/controllers/balance.js
@@ -7,19 +7,23 @@ exports.computeBalance = (req, res, next) => {
       var factor = 0;
       var share = 0;
       var by = "";
-      transactions.forEach((transaction) => {
-        balance.forEach((user) => {
-          by = user.keys()[0];
-          if (by === transaction.by) {
-            factor = 1;
-            share = (transaction.for.length - 1) / transaction.for.length;
-          } else {
-            factor = -1;
-            share = 1 / transaction.for.length;
-          }
-          balance[by] = balance[by] + factor * share * transaction.amount;
-        });
-      });
+      transactions
+        .forEach((transaction) => {
+          balance
+            .forEach((user) => {
+              by = user.keys()[0];
+              if (by === transaction.by) {
+                factor = 1;
+                share = (transaction.for.length - 1) / transaction.for.length;
+              } else {
+                factor = -1;
+                share = 1 / transaction.for.length;
+              }
+              balance[by] = balance[by] + factor * share * transaction.amount;
+            })
+            .catch((error) => res.status(400).json({ error }));
+        })
+        .catch((error) => res.status(400).json({ error }));
       res.status(200).json(balance);
     })
     .catch((error) => res.status(400).json({ error }));
