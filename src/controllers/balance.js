03c0fd917c8750eb4336d2755d2f479096647f67
PireSvyt <81512700+PireSvyt@users.noreply.github.com>
2022-04-01 17:56:08 +0200

    with error message

--- src/controllers/balance.js
+++ src/controllers/balance.js
@@ -3,13 +3,13 @@ const Transaction = require("../models/Transaction");
 exports.computeBalance = (req, res, next) => {
   Transaction.find()
     .then((transactions) => {
-      var balance = [{ Alice: 0 }, { Pierre: 0 }];
+      var users = [{ Alice: 0 }, { Pierre: 0 }];
       var factor = 0;
       var share = 0;
       var by = "";
       transactions
         .forEach((transaction) => {
-          balance
+          users
             .forEach((user) => {
               by = user.keys()[0];
               if (by === transaction.by) {
@@ -19,12 +19,25 @@ exports.computeBalance = (req, res, next) => {
                 factor = -1;
                 share = 1 / transaction.for.length;
               }
-              balance[by] = balance[by] + factor * share * transaction.amount;
+              users[by] = users[by] + factor * share * transaction.amount;
             })
-            .catch((error) => res.status(400).json({ error }));
+            .catch((error) =>
+              res
+                .status(400)
+                .json({ message: "problème de boucle dans les users", error })
+            );
         })
-        .catch((error) => res.status(400).json({ error }));
-      res.status(200).json(balance);
+        .catch((error) =>
+          res
+            .status(400)
+            .json({
+              message: "problème de boucle dans les transactions",
+              error
+            })
+        );
+      res.status(200).json(users);
     })
-    .catch((error) => res.status(400).json({ error }));
+    .catch((error) =>
+      res.status(400).json({ message: "problème de recherche", error })
+    );
 };
