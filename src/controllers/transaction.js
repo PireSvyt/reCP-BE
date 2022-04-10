a80be1033eae03900efa34e214b96a6972ff53f8
PireSvyt <81512700+PireSvyt@users.noreply.github.com>
2022-04-10 13:00:43 +0200

    sorting reverse

--- src/controllers/transaction.js
+++ src/controllers/transaction.js
@@ -52,8 +52,8 @@ exports.findOneTransaction = (req, res, next) => {
 
 exports.findTransactions = (req, res, next) => {
   function compare(a, b) {
-    if (a.date < b.date) return -1;
-    if (a.date > b.date) return 1;
+    if (a.date > b.date) return -1;
+    if (a.date < b.date) return 1;
     // a doit être égal à b
     return 0;
   }
