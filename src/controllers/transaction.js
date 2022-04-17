d2477d9374406da5ceef0d57ca98df3164c5fc62
PireSvyt <81512700+PireSvyt@users.noreply.github.com>
2022-04-17 12:31:11 +0200

    adding transaction category management on be side

--- src/controllers/transaction.js
+++ src/controllers/transaction.js
@@ -1,26 +1,57 @@
+//const categorytransactionAPI = require("./categorytransaction");
 const Transaction = require("../models/Transaction");
+const CategoryTransaction = require("../models/CategoryTransaction");
 
 exports.createTransaction = (req, res, next) => {
   delete req.body._id;
   const transaction = new Transaction({ ...req.body });
-  transaction
-    .save()
-    .then(() => {
-      res.status(201).json({
-        message: "transaction enregistrée",
-        id: transaction._id
-      });
-    })
-    .catch((error) => res.status(400).json({ error }));
+  saveCategory(transaction.category).then((id) => {
+    transaction.category = id;
+    transaction
+      .save()
+      .then(() => {
+        res.status(201).json({
+          message: "transaction enregistrée",
+          id: transaction._id
+        });
+      })
+      .catch((error) => res.status(400).json({ error }));
+  });
 };
 
+async function saveCategory(category) {
+  return CategoryTransaction.find().then((categories) => {
+    let categoryObject = categories.filter(function (value, index, arr) {
+      return value.name === category;
+    });
+    if (categoryObject.length === 1) {
+      return categoryObject[0]._id;
+    } else {
+      const categorytransaction = new CategoryTransaction({
+        name: category
+      });
+      categorytransaction.save().then((categoryCreationRes) => {
+        if (categoryCreationRes.status === 200) {
+          return categoryCreationRes.id;
+        } else {
+          return "";
+        }
+      });
+    }
+  });
+}
+
 exports.modifyTransaction = (req, res, next) => {
-  Transaction.updateOne(
-    { _id: req.params.id },
-    { ...req.body, _id: req.params.id }
-  )
-    .then(() => res.status(200).json({ message: "transaction modifiée" }))
-    .catch((error) => res.status(400).json({ error }));
+  let transaction = new Transaction({ ...req.body });
+  saveCategory(transaction.category).then((id) => {
+    transaction.category = id;
+    Transaction.updateOne(
+      { _id: req.params.id },
+      { transaction, _id: req.params.id }
+    )
+      .then(() => res.status(200).json({ message: "transaction modifiée" }))
+      .catch((error) => res.status(400).json({ error }));
+  });
 };
 
 exports.deleteTransaction = (req, res, next) => {
@@ -43,8 +74,29 @@ exports.deleteTransaction = (req, res, next) => {
 };
 
 exports.findOneTransaction = (req, res, next) => {
+  //console.log("findOneTransaction req");
+  //console.log(req);
   Transaction.findOne({ _id: req.params.id })
-    .then((transaction) => res.status(200).json(transaction))
+    .then((transaction) => {
+      //console.log("transaction");
+      //console.log(transaction);
+      if (transaction.category.match(/^[a-zA-Z0-9]+$/) === null) {
+        //console.log("transaction.category");
+        //console.log(transaction.category);
+        res.status(200).json(transaction);
+      } else {
+        CategoryTransaction.findOne({ _id: transaction.category })
+          .then((category) => {
+            //console.log("category");
+            //console.log(category);
+            transaction.category = category.name;
+            res.status(200).json(transaction);
+          })
+          .catch((error) =>
+            res.status(404).json({ message: "catégorie introuvable" })
+          );
+      }
+    })
     .catch((error) =>
       res.status(404).json({ message: "transaction introuvable" })
     );
--- src/models/CategoryTransaction.js
+++ src/models/CategoryTransaction.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const categorytransactionSchema = mongoose.Schema({
-  name: { type: String, required: true }
+  name: { type: String, required: true, unique: true }
 });
+
+categorytransactionSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model(
   "CategoryTransaction",
