3d5cfe2e17a5d2ee60015eaa72996c7cec1ff2e0
pst <81512700+PireSvyt@users.noreply.github.com>
2024-09-03 10:30:03 +0000

    authHeader

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,6 +5,6 @@ const authAuthenticate = require("../controllers/auth/authAuthenticate.js");
 
 const userGetMe = require("../controllers/user/userGetMe.js");
 
-router.post("/v1/getme", authAuthenticate, userGetMe);
+router.get("/v1/getme", authAuthenticate, userGetMe);
 
 module.exports = router;
