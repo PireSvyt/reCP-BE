9e913fdc28c9ee86df3f034ed0b4e2fa95987650
pst <81512700+PireSvyt@users.noreply.github.com>
2024-09-06 21:31:05 +0000

    debug

--- src/controllers/compute/services/computeTransactionCurve.js
+++ src/controllers/compute/services/computeTransactionCurve.js
@@ -22,11 +22,15 @@ module.exports = function computeTransactionCurve(transactions, need) {
   transactions.forEach((transaction) => {
     let slice = Math.floor(
       (Date.parse(transaction.date) - Date.parse(need.since)) /
-        (1000 * 3600 * 24) /
-        need.by
+        (1000 * 3600 * 24 * need.by)
     );
     if (Object.keys(curve).includes(slice)) {
       curve[slice].total += transaction.amount;
+    } else {
+      console.log("Date.parse(transaction.date)", Date.parse(transaction.date));
+      console.log("Date.parse(need.since)", Date.parse(need.since));
+      console.log("1000*3600*24*need.by", 1000 * 3600 * 24 * need.by);
+      console.log("slice", slice);
     }
   });
 
