1842577c8f438ffd71b7c9ce8ce21dc20dd469b9
pst <81512700+PireSvyt@users.noreply.github.com>
2024-09-06 21:40:05 +0000

    Difference_In_Days

--- src/controllers/compute/services/computeTransactionCurve.js
+++ src/controllers/compute/services/computeTransactionCurve.js
@@ -16,20 +16,28 @@ module.exports = function computeTransactionCurve(transactions, need) {
       date: curveDate,
     };
   }
-  console.log("curve", curve);
 
   // Totalise transactions
   transactions.forEach((transaction) => {
-    let slice = Math.floor(
+    let Difference_In_Time =
+      new Date.parse(transaction.date).getTime() -
+      new Date.parse(need.since).getTime();
+
+    let Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+    let Period = Difference_In_Days / need.by;
+    let slice = Match.floor(Period);
+
+    /*let slice = Math.floor(
       (Date.parse(transaction.date) - Date.parse(need.since)) /
         (1000 * 3600 * 24 * need.by)
-    );
+    );*/
+
     if (Object.keys(curve).includes(slice)) {
-      curve[slice].total += transaction.amount;
+      curve[slice].total = curve[slice].total + transaction.amount;
     } else {
-      console.log("Date.parse(transaction.date)", Date.parse(transaction.date));
-      console.log("Date.parse(need.since)", Date.parse(need.since));
-      console.log("1000*3600*24*need.by", 1000 * 3600 * 24 * need.by);
+      console.log("Difference_In_Time", Difference_In_Time);
+      console.log("Difference_In_Days", Difference_In_Days);
+      console.log("Period", Period);
       console.log("slice", slice);
     }
   });
