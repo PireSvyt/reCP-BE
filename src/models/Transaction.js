10a55e707e933f28ae5e3f03d1cc4fceb5e54283
pst <81512700+PireSvyt@users.noreply.github.com>
2024-09-07 18:05:38 +0000

    TagidSchema

--- src/models/Transaction.js
+++ src/models/Transaction.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-//const TagidSchema = new mongoose.Schema({ type: String });
+const TagidSchema = new mongoose.Schema({ tagid: { type: String } });
 const transactionSchema = mongoose.Schema(
   {
     schema: { type: String },
@@ -13,7 +13,7 @@ const transactionSchema = mongoose.Schema(
     for: [{ type: String }],
     categoryid: { type: String },
     tagids: {
-      type: Array,
+      type: [TagidSchema],
       required: false,
     },
   },
