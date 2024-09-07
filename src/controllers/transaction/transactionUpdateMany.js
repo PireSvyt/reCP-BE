ff203b0e8bd8bb78f13c2f3d143cb04216657a84
pst <81512700+PireSvyt@users.noreply.github.com>
2024-09-07 15:54:36 +0000

    transactionUpdateMany

---
+++ src/controllers/transaction/transactionUpdateMany.js
@@ -0,0 +1,76 @@
+require("dotenv").config();
+const Transaction = require("../../models/Transaction.js");
+
+module.exports = transactionUpdateMany = (req, res, next) => {
+  /*
+  
+  update transactions
+  
+  possible response types
+  * transaction.updatemany.success
+  * transaction.updatemany.error
+  
+  possible edits
+  * - categoryid
+  
+  */
+
+  if (process.env.DEBUG) {
+    console.log("transaction.updatemany");
+  }
+
+  // Initialize
+  var status = 500;
+  var type = "transaction.updatemany.error";
+  var filters = {};
+  var changes = {};
+
+  // Is need input relevant?
+  if (!req.body.need) {
+    status = 403;
+    type = "transaction.updatemany.error.noneed";
+  } else {
+    switch (req.body.need) {
+      case "categoryid":
+        filters = { transactionid: { $in: req.body.transactions } };
+        changes = { categoryid: req.body.categoryid };
+        break;
+      default:
+        status = 403;
+        type = "transaction.updatemany.error.needmissmatch";
+    }
+  }
+
+  // Update
+  Transaction.updateMany(filters, changes)
+    .then((outcome) => {
+      if (outcome.nModified === req.body.transactions.length) {
+        console.log("transaction.updatemany.success");
+        return res.status(201).json({
+          type: "transaction.updatemany.success",
+          data: {
+            outcome: outcome,
+          },
+        });
+      } else {
+        console.log("transaction.updatemany.partial");
+        return res.status(202).json({
+          type: "transaction.updatemany.partial",
+          data: {
+            outcome: outcome,
+          },
+        });
+      }
+    })
+    .catch((error) => {
+      console.log("transaction.updatemany.error");
+      console.error(error);
+      return res.status(400).json({
+        type: "transaction.updatemany.error",
+        error: error,
+        data: {
+          outcome: null,
+        },
+      });
+    });
+};
--- src/routes/transaction.js
+++ src/routes/transaction.js
@@ -8,31 +8,13 @@ const transactionSave = require("../controllers/transaction/transactionSave.js")
 const transactionDelete = require("../controllers/transaction/transactionDelete.js");
 const transactionGetOne = require("../controllers/transaction/transactionGetOne.js");
 const transactionGetList = require("../controllers/transaction/transactionGetList.js");
+const transactionUpdateMany = require("../controllers/transaction/transactionUpdateMany.js");
 
-router.post(
-    "/v1/create",
-    authAuthenticate,
-    transactionCreate,
-);
-router.post(
-    "/v1/save",
-    authAuthenticate,
-    transactionSave,
-);
-router.delete(
-    "/v1/:transactionid",
-    authAuthenticate,
-    transactionDelete
-);
-router.get(
-    "/v1/:transactionid",
-    authAuthenticate,
-    transactionGetOne
-);
-router.post(
-    "/v1/getlist",
-    authAuthenticate,
-    transactionGetList
-);
+router.post("/v1/create", authAuthenticate, transactionCreate);
+router.post("/v1/save", authAuthenticate, transactionSave);
+router.delete("/v1/:transactionid", authAuthenticate, transactionDelete);
+router.get("/v1/:transactionid", authAuthenticate, transactionGetOne);
+router.post("/v1/getlist", authAuthenticate, transactionGetList);
+router.post("/v1/updatemany", authAuthenticate, transactionUpdateMany);
 
 module.exports = router;
