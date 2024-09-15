2fb4197ec8f9e010218d373254682b5d31c95f85
pst <81512700+PireSvyt@users.noreply.github.com>
2024-09-15 10:27:01 +0000

    fixing computation

--- src/controllers/balance/balance.js
+++ src/controllers/balance/balance.js
@@ -19,78 +19,89 @@ exports.get = (req, res, next) => {
           name: category.name,
         };
       });
+      console.log("categories", categories);
 
       // Gather balance rules
       let balancerules = [];
-      BalanceRule.find().then((balanceruleList) => {
-        balancerules = balanceruleList;
-        balancerules = balancerules.sort((a, b) => {
-          return a.startdate - b.startdate;
-        });
-
-        // Gather transactions
-        Transaction.find()
-          .then((transactions) => {
-            var users = { Alice: 0, Pierre: 0 };
-            var jsonTransaction = {};
-            var categoryTotal = 0;
-            var categoryUndefined = 0;
-            var transactionUserBalance = null;
-            transactions.forEach((transaction) => {
-              jsonTransaction = transaction.toObject();
-              // Balance per user
-              transactionUserBalance = computeTransactionBalance(
-                jsonTransaction,
-                balancerules
-              );
-              for (var user of Object.keys(transactionUserBalance)) {
-                users[user] += transactionUserBalance[user];
-              }
-              // Balance per category
-              if (jsonTransaction.categoryid !== "") {
-                if (categories[jsonTransaction.categoryid] !== undefined) {
-                  categories[jsonTransaction.categoryid].total +=
-                    jsonTransaction.amount;
-                }
-              } else {
-                categoryUndefined += jsonTransaction.amount;
-              }
-              categoryTotal += jsonTransaction.amount;
-            });
-            // Adding category undefiend and total
-            categories["-"] = {
-              name: "-",
-              categoryid: "-",
-              total: categoryUndefined,
-            };
-            categories["Total"] = {
-              name: "Total",
-              categoryid: "Total",
-              total: categoryTotal,
-            };
-            // Sort categories
-            let orderedCategories = sortObject(categories, "total");
-            // Merge
-            status = 200; // OK
-            res.status(status).json({
-              status: status,
-              message: "summary ok",
-              summary: { users: users, categories: orderedCategories },
-            });
-          })
-          .catch((error) => {
-            status = 400; // OK
-            res.status(status).json({
-              status: status,
-              message: "error on find transactions",
-              summary: {},
-              error: error,
-            });
-            console.error(error);
+      BalanceRule.find()
+        .then((balanceruleList) => {
+          balancerules = [...balanceruleList];
+          balancerules = balancerules.sort((a, b) => {
+            return a.startdate - b.startdate;
           });
-          
+          console.log("balance rules", balancerules);
+
+          // Gather transactions
+          Transaction.find()
+            .then((transactions) => {
+              var users = { Alice: 0, Pierre: 0 };
+              var jsonTransaction = {};
+              var categoryTotal = 0;
+              var categoryUndefined = 0;
+              var transactionUserBalance = null;
+              transactions.forEach((transaction) => {
+                jsonTransaction = transaction.toObject();
+                // Balance per user
+                transactionUserBalance = computeTransactionBalance(
+                  jsonTransaction,
+                  balancerules
+                );
+                for (var user of Object.keys(transactionUserBalance)) {
+                  users[user] += transactionUserBalance[user];
+                }
+                // Balance per category
+                if (jsonTransaction.categoryid !== "") {
+                  if (categories[jsonTransaction.categoryid] !== undefined) {
+                    categories[jsonTransaction.categoryid].total +=
+                      jsonTransaction.amount;
+                  }
+                } else {
+                  categoryUndefined += jsonTransaction.amount;
+                }
+                categoryTotal += jsonTransaction.amount;
+              });
+              // Adding category undefiend and total
+              categories["-"] = {
+                name: "-",
+                categoryid: "-",
+                total: categoryUndefined,
+              };
+              categories["Total"] = {
+                name: "Total",
+                categoryid: "Total",
+                total: categoryTotal,
+              };
+              // Sort categories
+              let orderedCategories = sortObject(categories, "total");
+              // Merge
+              status = 200; // OK
+              res.status(status).json({
+                status: status,
+                message: "summary ok",
+                summary: { users: users, categories: orderedCategories },
+              });
+            })
+            .catch((error) => {
+              status = 400; // OK
+              res.status(status).json({
+                status: status,
+                message: "error on find transactions",
+                summary: {},
+                error: error,
+              });
+              console.error(error);
+            });
+        })
+        .catch((error) => {
+          status = 400; // OK
+          res.status(status).json({
+            status: status,
+            message: "error on find balance rules",
+            summary: {},
+            error: error,
+          });
+          console.error(error);
         });
-      
     })
     .catch((error) => {
       status = 400; // OK
--- src/controllers/balance/services/computeTransactionBalance.js
+++ src/controllers/balance/services/computeTransactionBalance.js
@@ -5,32 +5,33 @@ module.exports = function computeTransactionBalance(transaction, balancerules) {
   };
 
   // Any rule applying?
-  balancerules.forEach(balancerule => {
-    let useRuleRatio = true
-    if (Date.parse(transaction.date) < Date.parse(balancerule.startdate)) { 
-      useRuleRatio = false
+  balancerules.forEach((balancerule) => {
+    let useRuleRatio = true;
+    if (Date.parse(transaction.date) < Date.parse(balancerule.startdate)) {
+      useRuleRatio = false;
     }
-    if (balancerule.categories
-      .filter(cat => {
-        return cat.categoryid === transaction.categoryid
-      }).length !== 1 ) {
-      useRuleRatio = false
+    if (
+      balancerule.categories.filter((cat) => {
+        return cat.categoryid === transaction.categoryid;
+      }).length !== 1
+    ) {
+      useRuleRatio = false;
     }
     if (balancerule.enddate !== undefined) {
-      if (Date.parse(balancerule.enddate) < Date.parse(transaction.date)) {      
-        useRuleRatio = false
+      if (Date.parse(balancerule.enddate) < Date.parse(transaction.date)) {
+        useRuleRatio = false;
       }
     }
     if (useRuleRatio) {
-      console.log("using rule",balancerule,"for transaction",transaction)
+      console.log("using rule", balancerule, "for transaction", transaction);
       ratio.Alice = balancerule.filter((br) => {
-          return br.user === "Alice";
-        })[0].ratio      
+        return br.user === "Alice";
+      })[0].ratio;
       ratio.Pierre = balancerule.filter((br) => {
-          return br.user === "Pierre";
-        })[0].ratio
+        return br.user === "Pierre";
+      })[0].ratio;
     }
-  }
+  });
 
   // Balance
   let outcome = {};
@@ -52,7 +53,5 @@ module.exports = function computeTransactionBalance(transaction, balancerules) {
     }
   }
 
-  if(Alice.ratio === 0.5) { console.log("used default ratio") }
-  
   return outcome;
 };
