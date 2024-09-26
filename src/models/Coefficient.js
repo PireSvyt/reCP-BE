9ba3ba7facd124cde940a8c76f9f597127103aa8
PireSvyt <81512700+PireSvyt@users.noreply.github.com>
2024-09-26 08:41:51 +0200

    Update Coefficient.js

--- src/models/Coefficient.js
+++ src/models/Coefficient.js
@@ -2,15 +2,15 @@ const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
 const coefficientSchema = mongoose.Schema(
-	{
-		schema: { type: String },
-		coefficientid: { type: String, required: true, unique: true },
-		startdate: { type: Date, required: true },
-		enddate: { type: Date },
-		categoryids:  [{ type: String }],
-		userratios: {type: Map, of : Number},
-	},
-	{ strict: true }
+{
+schema: { type: String },
+coefficientid: { type: String, required: true, unique: true },
+startdate: { type: Date, required: true },
+enddate: { type: Date },
+categoryids:  [{ type: String }],
+userratios: {type: Map, of : Number},
+},
+{ strict: true }
 );
 
 coefficientSchema.plugin(uniqueValidator);
