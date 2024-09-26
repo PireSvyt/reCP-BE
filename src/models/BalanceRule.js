e4501a7b426e3663cd0ebfda67e41c25c3f5ddba
PireSvyt <81512700+PireSvyt@users.noreply.github.com>
2024-09-26 07:38:49 +0200

    Update BalanceRule.js

--- src/models/BalanceRule.js
+++ src/models/BalanceRule.js
@@ -1,14 +1,16 @@
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
+mongoose.set("debug", true);
+
 const balanceruleSchema = mongoose.Schema(
 	{
 		schema: { type: String },
 		balanceruleid: { type: String, required: true, unique: true },
 		startdate: { type: Date, required: true },
 		enddate: { type: Date },
-		//categoryids:  [ String ],
-		//userratios: {type: Map, of : Number},
+		categoryids:  [ String ],
+		userratios: {type: Map, of : Number},
 	},
 	{ strict: true }
 );
