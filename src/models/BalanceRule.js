da7b834e54882c2822164b9341d6af5ee57bfa78
PireSvyt <81512700+PireSvyt@users.noreply.github.com>
2024-09-26 08:07:58 +0200

    Update BalanceRule.js

--- src/models/BalanceRule.js
+++ src/models/BalanceRule.js
@@ -3,22 +3,18 @@ const uniqueValidator = require("mongoose-unique-validator");
 
 mongoose.set("debug", true);
 
-const baseSchema = new mongoose.Schema({}, { discriminatorKey: 'type' });
-const BaseModel = mongoose.model('Test', baseSchema);
-
-const balanceruleSchema = mongoose.Schema(
+const balanceruleSchema = new mongoose.Schema(
 	{
 		schema: { type: String },
 		balanceruleid: { type: String, required: true, unique: true },
 		startdate: { type: Date, required: true },
 		enddate: { type: Date },
-		categoryids:  [ String ],
-		//userratios: {type: Map, of : Number},
+		categoryids:  [{ type: String }],
+		userratios: {type: Map, of : Number},
 	},
 	{ strict: true }
 );
+
 balanceruleSchema.plugin(uniqueValidator);
 
-const BalanceRule = BaseModel.discriminator('balancerule', balanceruleSchema);
-
-module.exports = BalanceRule
+module.exports = mongoose.model("BalanceRule", balanceruleSchema);
