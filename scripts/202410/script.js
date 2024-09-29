4c3578a89d60d76043ebc1d957c8f7ff695a793c
PireSvyt <81512700+PireSvyt@users.noreply.github.com>
2024-09-29 13:00:57 +0000

    f***

--- scripts/202410/script.js
+++ scripts/202410/script.js
@@ -12,48 +12,52 @@ let migrations = [];
 migrations.push({
   name: "encryption users",
   collections: ["users"],
-  mapping: (item) => {
-    let mappedItem = { ...item };
-    console.log("userid", mappedItem.userid)
+  mapping: async (item) => {
+    return await new Promise ((resolve, reject) => {
 
-    // Mapping
-    delete mappedItem.__v;
+      let mappedItem = { ...item };
+      console.log("userid", mappedItem.userid)
 
-    /*  0. add a schema reference to original data */
-    mappedItem.schema = "202410";
+      // Mapping
+      delete mappedItem.__v;
+      delete mappedItem.login;
+      delete mappedItem.name;
 
-    /*  1. encrypt login and name for all items */
-    mappedItem.state = "active"
-    let encryptedLogin = CryptoJS.AES.encrypt(
-      mappedItem.login,
-      inputs.ENCRYPTION_KEY
-    )
-    console.log("login", mappedItem.login)
-    console.log("encryptedLogin", encryptedLogin.toString(CryptoJS.enc.Utf8))
-    mappedItem.login = encryptedLogin.toString(CryptoJS.enc.Utf8)
-    encryptedName = CryptoJS.AES.encrypt(
-      mappedItem.name,
-      inputs.ENCRYPTION_KEY
-    )
-    console.log("name", mappedItem.name)
-    console.log("encryptedName", encryptedName.toString(CryptoJS.enc.Utf8))
-    mappedItem.name = encryptedName.toString(CryptoJS.enc.Utf8)
+      /*  0. add a schema reference to original data */
+      mappedItem.schema = "202410";
 
-    // Return
-    return mappedItem;
+      /*  1. encrypt login and name for all items */
+      mappedItem.state = "active"
+      let encryptedLogin = CryptoJS.AES.encrypt(
+        item.login,
+        inputs.ENCRYPTION_KEY
+      )
+      console.log("encryptedLogin", encryptedLogin.toString(CryptoJS.enc.Utf8))
+      mappedItem.login = encryptedLogin.toString(CryptoJS.enc.Utf8)
+      encryptedName = CryptoJS.AES.encrypt(
+        item.name,
+        inputs.ENCRYPTION_KEY
+      )
+      console.log("encryptedName", encryptedName.toString(CryptoJS.enc.Utf8))
+      mappedItem.name = encryptedName.toString(CryptoJS.enc.Utf8)
 
-  },
+      // Return
+      //return mappedItem;
+      resolve(mappedItem);
+      
+    })
+  }
 });
 
 
 let state = {
-collections: ["users"],
-origin: {
-users: usersOrigin,
-},
-destination: {
-users: [],
-},
+  collections: ["users"],
+  origin: {
+    users: usersOrigin,
+  },
+  destination: {
+    users: [],
+  },
 };
 
 
@@ -64,80 +68,79 @@ async function script() {
   
   // MAPPING ----------------------------------------------------------------------------------------------------------
   console.log("\n> MAPPING" + "\n");
-  applyMigrations(migrations).then(async () => {
+  let foo = await applyMigrations(migrations)
   
-    // DESTINATION EXPORT -----------------------------------------------------------------------------------------------
-    console.log("\n> DESTINATION EXPORT" + "\n");
-    exportCollections().then(async () => {
-      console.log("\nSCRIPT END");
-    })
-  })
+  // DESTINATION EXPORT -----------------------------------------------------------------------------------------------
+  console.log("\n> DESTINATION EXPORT" + "\n");
+  foo = await exportCollections()
+  
+  console.log("\nSCRIPT END");
+   
 }
 
 async function exportCollections() {
-try {
-state.collections.forEach(async (collection) => {
-console.log("\n==> collection : " + collection);
-let collectionFileName = "destination." + collection + ".json";
-let collectionFileLoc = path.join(
-__dirname,
-"/destination/",
-collectionFileName
-);
-console.log("collectionFileLoc", collectionFileLoc);
-await fs.ensureFile(collectionFileLoc).catch((err) => console.log);
-let collectionStream = fs.createWriteStream(collectionFileLoc, {
-encoding: "binary",
-flags: "w",
-});
-let collectionBuffer = Buffer.from(
-JSON.stringify(state.destination[collection], undefined, 2)
-);
-collectionStream.write(
-collectionBuffer,
-"binary"
-//e=>console.log('script mapped object ' + fileName.split(".")[0] + ' in ' + objectFileName + '\n')
-);
-});
-} catch (error) {
-console.error(error);
-console.log("/!\\  exportCollections : ", error);
-}
+  try {
+    state.collections.forEach(async (collection) => {
+      console.log("\n==> collection : " + collection);
+      let collectionFileName = "destination." + collection + ".json";
+      let collectionFileLoc = path.join(
+        __dirname,
+        "/destination/",
+        collectionFileName
+      );
+      console.log("collectionFileLoc", collectionFileLoc);
+      await fs.ensureFile(collectionFileLoc).catch((err) => console.log);
+      let collectionStream = fs.createWriteStream(collectionFileLoc, {
+        encoding: "binary",
+        flags: "w",
+      });
+      let collectionBuffer = Buffer.from(
+        JSON.stringify(state.destination[collection], undefined, 2)
+      );
+      collectionStream.write(
+        collectionBuffer,
+        "binary"
+        //e=>console.log('script mapped object ' + fileName.split(".")[0] + ' in ' + objectFileName + '\n')
+      );
+    });
+  } catch (error) {
+    console.error(error);
+    console.log("/!\\  exportCollections : ", error);
+  }
 }
 
 async function applyMigrations(migrations) {
-state.collections.forEach(async (collection) => {
-console.log("\n==> collection : " + collection);
-if (state.origin[collection] !== undefined) {
-state.origin[collection].forEach(async (item) => {
-let migratedItem = { ...item };
-migrations
-.filter((migration) => {
-//console.log("migration", migration)
-return (
-migration.collections.filter((migrationCollection) => {
-return migrationCollection === collection;
-}).length > 0
-);
-})
-.forEach(async (migration) => {
-//console.log("    migration : " + migration.name);
-migratedItem = migration.mapping(migratedItem)
-// Adding destination collection
-state.destination[collection].push(migratedItem);
-});
-});
-// Comparison
-console.log(
-"\n====> state.origin." + collection + "[0]" + "\n",
-state.origin[collection][0]
-);
-console.log(
-"\n====> state.destination." + collection + "[0]" + "\n",
-state.destination[collection][0]
-);
-}
-});
+  state.collections.forEach(async (collection) => {
+    console.log("\n==> collection : " + collection);
+    if (state.origin[collection] !== undefined) {
+      state.origin[collection].forEach(async (item) => {
+        let migratedItem = { ...item };
+        
+        migrations.filter((migration) => {
+          //console.log("migration", migration)
+          return (
+            migration.collections.filter((migrationCollection) => {
+              return migrationCollection === collection;
+            }).length > 0
+          );
+        }).forEach(async (migration) =>  {
+          migration.mapping(migratedItem).then((newItem) => {
+            // Adding destination collection
+            state.destination[collection].push(newItem);
+          })
+        })
+      });
+    // Comparison
+    /*console.log(
+      "\n====> state.origin." + collection + "[0]" + "\n",
+      state.origin[collection][0]
+    );*/
+    console.log(
+      "\n====> state.destination." + collection + "[0]" + "\n",
+      state.destination[collection][0]
+    );
+    }
+  });
 }
 
 script();
