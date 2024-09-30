25041a451db8ed0b1075344f45da1f348181166a
PireSvyt <81512700+PireSvyt@users.noreply.github.com>
2024-09-30 11:51:15 +0000

    rework

--- src/controllers/user/userCreate.js
+++ src/controllers/user/userCreate.js
@@ -18,28 +18,36 @@ if (process.env.DEBUG) {
 console.log("user.create");
 }
 
-const userToSave = { ...req.body };
-if (userToSave.userid === undefined) {
+const userToSave = {}
+if (eq.body.userid === undefined) {
     userToSave.userid = random_string(24)
+} else {
+    userToSave.userid = req.body.userid
 }
-if (userToSave.type === undefined) {
-    userToSave.type = "user"
-}
-userToSave.state = "inactive"
-userToSave.password = "TO RESET"
 if (req.body.encryption === false) {
-    if (userToSave.name !== undefined) {
     userToSave.name = CryptoJS.AES.decrypt(
-        userToSave.name,
+        req.body.name,
         process.env.ENCRYPTION_KEY
     ).toString(CryptoJS.enc.Utf8);
-    }
-    if (userToSave.login !== undefined) {
     userToSave.login = CryptoJS.AES.decrypt(
-        userToSave.login,
+        req.body.login,
         process.env.ENCRYPTION_KEY
     ).toString(CryptoJS.enc.Utf8);
-    }
+}
+if (req.body.type === undefined) {
+    userToSave.type = "user"
+} else {
+    userToSave.type = req.body.type
+}
+if (req.body.state === undefined) {
+    userToSave.state = "inactive"
+} else {
+    userToSave.state = req.body.state
+}
+if (req.body.password === undefined) {
+    userToSave.password = "TO RESET"
+} else {
+    userToSave.password = req.body.password
 }
 console.log("userToSave", userToSave)
 userToSave = new User(userToSave)
