d7c9c8b71bc3cfd1504a669f31464e905d820686
PireSvyt <81512700+PireSvyt@users.noreply.github.com>
2024-10-14 09:58:29 +0000

    encryptedData

--- src/utils/fieldEncrypt.js
+++ src/utils/fieldEncrypt.js
@@ -6,10 +6,10 @@ const iv = crypto.randomBytes(16);
 
 module.exports = function fieldEncrypt(field) {
   const cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
-  let encrypted = cipher.update(text, 'utf-8', 'hex');
+  let encrypted = cipher.update(field, 'utf-8', 'hex');
   encrypted += cipher.final('hex');
   console.log("field", field)
   console.log("iv.toString('hex')", iv.toString('hex'))
-  console.log("encryptedData", encryptedData)
+  console.log("encrypted", encrypted)
   return encrypted
 };
