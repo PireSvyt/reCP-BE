7859501e24fb02af248fdae74f046bd498e26e44
PireSvyt <81512700+PireSvyt@users.noreply.github.com>
2024-10-14 10:06:53 +0000

    oopsy

--- src/utils/fieldDecrypt.js
+++ src/utils/fieldDecrypt.js
@@ -5,10 +5,14 @@ const key = crypto.createHash('sha256').update(String(process.env.ENCRYPTION_KEY
 const inputIV = process.env.ENCRYPTION_IV;
 
 module.exports = function fieldDecrypt(encryptedData) {
-  const decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), Buffer.from(inputIV, 'hex'));
-  let decrypted = decipher.update(encryptedData, 'hex', 'utf-8');
-  decrypted += decipher.final('utf-8');
-  console.log("encryptedData", encryptedData)
-  console.log("decrypted", decrypted)
-  return decrypted;
+  try {
+    const decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), Buffer.from(inputIV, 'hex'));
+    let decrypted = decipher.update(encryptedData, 'hex', 'utf-8');
+    decrypted += decipher.final('utf-8');
+    console.log("encryptedData", encryptedData)
+    console.log("decrypted", decrypted)
+    return decrypted;
+  } catch {
+    return "oopsy"
+  }
 };
