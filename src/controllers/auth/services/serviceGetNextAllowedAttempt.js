a45adb223f41d0903a0d3a2587c37daf9e0e5ff7
PireSvyt <81512700+PireSvyt@users.noreply.github.com>
2024-10-19 09:48:30 +0000

    console.log("failedconnection", nowDate - Date.parse(failedconnection))

--- src/controllers/auth/services/serviceGetNextAllowedAttempt.js
+++ src/controllers/auth/services/serviceGetNextAllowedAttempt.js
@@ -10,22 +10,25 @@ module.exports = function serviceGetNextAllowedAttempt(failedconnections) {
 			let nowDate = Date.now()
 			
 			// No more than 5 attemps per minutes
-			let failedconnections1min = failedconnections.filter(failedconnection => {	
-				return (nowDate - Date.parse(failedconnection) < (1 * 1000 * 3600))
+			let failedconnections1min = failedconnections.filter(failedconnection => {
+				console.log("failedconnection", nowDate - failedconnection)
+				console.log("failedconnection", nowDate - Date.parse(failedconnection))
+				console.log("(1 * 1000 * 60)", (1 * 1000 * 60))
+				return (nowDate - Date.parse(failedconnection) < (1 * 1000 * 60))
 			})
 			if (5 <= failedconnections1min.length) {
 				outcome.delayed = true
-				outcome.date = failedconnections1min[0] + (1 * 1000 * 3600)
+				outcome.date = failedconnections1min[0] + (1 * 1000 * 60)
 				// No notifications				
 			}
 			
 			// No more than 15 attempts per 15 minutes
 			let failedconnections15min = failedconnections.filter(failedconnection => {		
-				return (nowDate - Date.parse(failedconnection) < (15 * 1000 * 3600))
+				return (nowDate - Date.parse(failedconnection) < (15 * 1000 * 60))
 			})
 			if (15 <= failedconnections15min.length) {
 				outcome.delayed = true
-				outcome.date = failedconnections15min[0] + (15 * 1000 * 3600)
+				outcome.date = failedconnections15min[0] + (15 * 1000 * 60)
 				// Email notification to reset password
 				
 				
