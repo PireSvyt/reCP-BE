6454c60c2a31194d19f653ef3347b905efdc2187
PireSvyt <81512700+PireSvyt@users.noreply.github.com>
2024-10-19 09:43:12 +0000

    (15 * 1000 * 60)

--- src/controllers/auth/services/serviceGetNextAllowedAttempt.js
+++ src/controllers/auth/services/serviceGetNextAllowedAttempt.js
@@ -11,21 +11,21 @@ module.exports = function serviceGetNextAllowedAttempt(failedconnections) {
 			
 			// No more than 5 attemps per minutes
 			let failedconnections1min = failedconnections.filter(failedconnection => {	
-				return (nowDate - Date.parse(failedconnection) < 1 * (1000 * 60 * 60))
+				return (nowDate - Date.parse(failedconnection) < (1 * 1000 * 60))
 			})
 			if (5 <= failedconnections1min.length) {
 				outcome.delayed = true
-				outcome.date = failedconnections1min[0] + 1 * (1000 * 60 * 60)
+				outcome.date = failedconnections1min[0] + (1 * 1000 * 60)
 				// No notifications				
 			}
 			
 			// No more than 15 attempts per 15 minutes
 			let failedconnections15min = failedconnections.filter(failedconnection => {		
-				return (nowDate - Date.parse(failedconnection) < 15 * (1000 * 60 * 60))
+				return (nowDate - Date.parse(failedconnection) < (15 * 1000 * 60))
 			})
 			if (15 <= failedconnections15min.length) {
 				outcome.delayed = true
-				outcome.date = failedconnections15min[0] + 15 * (1000 * 60 * 60)	
+				outcome.date = failedconnections15min[0] + (15 * 1000 * 60)
 				// Email notification to reset password
 				
 				
