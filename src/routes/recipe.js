9b5268d071ad3da42fc1d2e1ba4df9a946ba9ba9
PireSvyt <81512700+PireSvyt@users.noreply.github.com>
2024-10-28 17:00:05 +0000

    recipe pick

--- src/routes/recipe.js
+++ src/routes/recipe.js
@@ -8,10 +8,12 @@ const recipeCreate = require("../controllers/recipe/recipeCreate.js");
 const recipeSave = require("../controllers/recipe/recipeSave.js");
 const recipeDelete = require("../controllers/recipe/recipeDelete.js");
 const recipeGetList = require("../controllers/recipe/recipeGetList.js");
+const recipePick = require("../controllers/recipe/recipePick.js");
 
 router.post("/v1/create", authAuthenticate, recipeCreate);
 router.post("/v1/save", authAuthenticate, recipeSave);
 router.delete("/v1/:recipeid", authAuthenticate, recipeDelete);
 router.post("/v1/getlist", authAuthenticate, recipeGetList);
+router.post("/v1/pick", authAuthenticate, recipePick);
 
 module.exports = router;
