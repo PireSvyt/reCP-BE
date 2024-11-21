aa501507897698036bd32901002adef40918535e
PireSvyt <81512700+PireSvyt@users.noreply.github.com>
2024-11-21 16:23:45 +0000

    encryption using model

--- src/utils/fieldEncrypt.js
+++ src/utils/fieldEncrypt.js
@@ -1,6 +1,7 @@
 const CryptoJS = require("crypto-js");
 const crypto = require('crypto');
 const fieldEncryption = require('mongoose-field-encryption');
+const Encryption = require("../../models/Encryption.js");
 
 module.exports = function fieldEncrypt(decryptedField, source) {
   let encryptedField
@@ -13,9 +14,12 @@ module.exports = function fieldEncrypt(decryptedField, source) {
       ).toString();
       break
     case "BE":
-      const defaultSaltGenerator = (secret) => crypto.randomBytes(16);
+      /*const defaultSaltGenerator = (secret) => crypto.randomBytes(16);
       const _hash = (secret) => crypto.createHash("sha256").update(secret).digest("hex").substring(0, 32);
-      encryptedField = fieldEncryption.encrypt(decryptedField, _hash(process.env.ENCRYPTION_KEY), defaultSaltGenerator);
+      encryptedField = fieldEncryption.encrypt(decryptedField, _hash(process.env.ENCRYPTION_KEY), defaultSaltGenerator);*/
+      const encryption = new Encryption({ field: decryptedField });
+      encryption.encryptFieldsSync();
+      encryptedField = encryption.field
       break
   }
 
