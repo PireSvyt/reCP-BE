03a144d572e6b8b5bf827f0c2eb639df994f8507
PireSvyt <81512700+PireSvyt@users.noreply.github.com>
2024-11-21 09:35:02 +0000

    test

--- src/utils/fieldDecrypt.js
+++ src/utils/fieldDecrypt.js
@@ -1,11 +1,22 @@
 const CryptoJS = require("crypto-js");
-//const crypto = require('crypto');
+const crypto = require('crypto');
+const fieldEncryption = require('mongoose-field-encryption');
 
 module.exports = function fieldDecrypt(encryptedField) {
-	let decryptedField = CryptoJS.AES.decrypt(
-		encryptedField,
-		process.env.ENCRYPTION_KEY
-	).toString(CryptoJS.enc.Utf8)
+	let decryptedField
+	
+	switch (source) {
+	  case "FE": 
+		decryptedField = CryptoJS.AES.decrypt(
+			encryptedField,
+			process.env.ENCRYPTION_KEY
+		).toString(CryptoJS.enc.Utf8)
+		break
+	  case "BE":
+		const _hash = (secret) => crypto.createHash("sha256").update(secret).digest("hex").substring(0, 32);
+		decryptedField = fieldEncryption.decrypt(encryptedField, _hash(process.env.ENCRYPTION_KEY))
+		break
+	}
 
 	/*let mykey = crypto.createDecipher('aes-128-cbc', process.env.ENCRYPTION_KEY);
 	let decryptedField = mykey.update(encryptedField, 'hex', 'utf8')
--- src/utils/fieldEncrypt.js
+++ src/utils/fieldEncrypt.js
@@ -1,11 +1,22 @@
 const CryptoJS = require("crypto-js");
-//const crypto = require('crypto');
+const crypto = require('crypto');
+const fieldEncryption = require('mongoose-field-encryption');
 
-module.exports = function fieldEncrypt(decryptedField) {
-  let encryptedField = CryptoJS.AES.encrypt(
-    decryptedField,
-    process.env.ENCRYPTION_KEY
-  ).toString();
+module.exports = function fieldEncrypt(decryptedField, source) {
+  let encryptedField
+  
+  switch (source) {
+    case "FE": 
+      encryptedField = CryptoJS.AES.encrypt(
+        decryptedField,
+        process.env.ENCRYPTION_KEY
+      ).toString();
+      break
+    case "BE":
+      const _hash = (secret) => crypto.createHash("sha256").update(secret).digest("hex").substring(0, 32);
+      encryptedField = fieldEncryption.encrypt(decryptedField, _hash(process.env.ENCRYPTION_KEY))
+      break
+  }
 
   /*let mykey = crypto.createCipher('aes-128-cbc', process.env.ENCRYPTION_KEY);
   let encryptedField = mykey.update(decryptedField, 'utf8', 'hex')
