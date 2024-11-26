cdac6bf8948d0c484e507bca90968d56eeed0a22
PireSvyt <81512700+PireSvyt@users.noreply.github.com>
2024-11-26 04:29:27 +0000

    match using function which decrypts

--- src/controllers/auth/authSignIn.js
+++ src/controllers/auth/authSignIn.js
@@ -33,146 +33,153 @@ module.exports = authSignIn = (req, res, next) => {
 
   let attemptLogin = fieldEncrypt(req.body.login, "BE")
 
-  let userToSearch = new User({
-	  userid: random_string(10),
-	  type: "user",
-	  state: "inactive",
-	  name: random_string(10),
-	  login: req.body.login,
-	  password: random_string(10),
-	  communityid: random_string(10),
-	  failedconnections:[],
-  })
-  userToSearch.encryptFieldsSync();
-  attemptLogin = userToSearch.login
-
-  User.findOne({ login: { $in : [ attemptLogin, req.body.login ] } })
+  User.aggregate([
+	{
+	  	addFields: {
+			decryptedLogin: {
+				$function: {
+					body: function decrypt(login){
+						return fieldDecrypt(login, "BE")
+					},
+					args: ["$login"],
+					lang: "js"
+				}
+			}
+		}
+	},
+	{
+	  $match: { decryptedLogin: { $in : [ attemptLogin, req.body.login ] } }
+	}
+  ])
+  //User.findOne({ login: { $in : [ attemptLogin, req.body.login ] } })
   //User.findOne({ login: attemptLogin })
-    .then((user) => {
-      if (!user) {
-        // Inexisting user
-        console.log("auth.signin.error.notfound");
+    .then((users) => {
+	  if (users.length !== 1) {
+		console.log("auth.signin.error.notfound");
         return res.status(404).json({
           type: "auth.signin.error.notfound",
         });
-      } else if (user.state === "anonymous") {
-        // Inexisting user
-        console.log("auth.signin.error.notfound");
-        return res.status(404).json({
-          type: "auth.signin.error.notfound",
-        });
-      } else {
-	      // Check attempts
-	      let nextAllowedAttempt = serviceGetNextAllowedAttempt(user.failedconnections)
-	      if (nextAllowedAttempt.delayed === true) {
-			// Deny access with delay
-			console.log("auth.signin.error.toomanyattempts");
-			return res.status(401).json({
-				type: "auth.signin.error.toomanyattempts",
-				nextattempt: nextAllowedAttempt.date,
+	  } else {
+		let user = users[0]
+		if (user.state === "anonymous") {
+			// Inexisting user
+			console.log("auth.signin.error.notfound");
+			return res.status(404).json({
+			type: "auth.signin.error.notfound",
 			});
-		  } else {	      
-			// Check password
-			let attemptPassword = fieldDecrypt(req.body.password, "FE")
-			bcrypt
-		        .compare(attemptPassword, user.password)
-		        .then((valid) => {
-		          if (!valid) {
-								console.log("bcrypt.compare", valid)
-		            // Account for attempt ?
-		            let failedconnections
-		            if (user.failedconnections === undefined) {
-			            failedconnections = [ Date.now() ]
-		            } else {
-			            failedconnections = [...user.failedconnections]
-			            failedconnections.push( Date.now() )
-		            }	            
-		            // Record failed attempt
-		            User.updateOne(
-									{ userid: user.userid },
-									{ "$set": { 
-										  failedconnections : failedconnections 
-									  }
-					        }
-								).then((outcome) => {
-									if (outcome.acknowledged) {
-									  console.log("user.recordfailedconnections.success");
-									} else {
-									  console.log("user.recordfailedconnections.failed");
-									}
-							  })
-							  .catch((error) => {
-									console.log("user.recordfailedconnections.error");
-									console.error(error);
-							  })
-							  .then(() => {
-								  nextAllowedAttempt = serviceGetNextAllowedAttempt(failedconnections)
-								  if (nextAllowedAttempt.delayed === true) {
-									  // Deny access with delay
-				            console.log("auth.signin.error.toomanyattempts");
-				            return res.status(401).json({
-				              type: "auth.signin.error.toomanyattempts",
-				              nextattempt: nextAllowedAttempt.date,
-				            });
-								  } else {
-				            // Only deny access
-				            console.log("auth.signin.error.invalidpassword");
-				            return res.status(401).json({
-				              type: "auth.signin.error.invalidpassword",
-				            });
-								  }
-							  })	            
-		          } else {	
-		            // Record connection & clear previous attempts
-					User.updateOne(
-						{ 
-							userid: user.userid },
-							{ "$set": { 
-								lastconnection : Date.now() ,
-								failedconnections: []
-							}
-						}
-					).then((outcome) => {
-						if (outcome.acknowledged) {
-							console.log("user.recordconnection.success");
+		} else {
+			// Check attempts
+			let nextAllowedAttempt = serviceGetNextAllowedAttempt(user.failedconnections)
+			if (nextAllowedAttempt.delayed === true) {
+				// Deny access with delay
+				console.log("auth.signin.error.toomanyattempts");
+				return res.status(401).json({
+					type: "auth.signin.error.toomanyattempts",
+					nextattempt: nextAllowedAttempt.date,
+				});
+			} else {	      
+				// Check password
+				let attemptPassword = fieldDecrypt(req.body.password, "FE")
+				bcrypt
+					.compare(attemptPassword, user.password)
+					.then((valid) => {
+					if (!valid) {
+									console.log("bcrypt.compare", valid)
+						// Account for attempt ?
+						let failedconnections
+						if (user.failedconnections === undefined) {
+							failedconnections = [ Date.now() ]
 						} else {
-							console.log("user.recordconnection.failed");
-						}
+							failedconnections = [...user.failedconnections]
+							failedconnections.push( Date.now() )
+						}	            
+						// Record failed attempt
+						User.updateOne(
+										{ userid: user.userid },
+										{ "$set": { 
+											failedconnections : failedconnections 
+										}
+								}
+									).then((outcome) => {
+										if (outcome.acknowledged) {
+										console.log("user.recordfailedconnections.success");
+										} else {
+										console.log("user.recordfailedconnections.failed");
+										}
+								})
+								.catch((error) => {
+										console.log("user.recordfailedconnections.error");
+										console.error(error);
+								})
+								.then(() => {
+									nextAllowedAttempt = serviceGetNextAllowedAttempt(failedconnections)
+									if (nextAllowedAttempt.delayed === true) {
+										// Deny access with delay
+								console.log("auth.signin.error.toomanyattempts");
+								return res.status(401).json({
+								type: "auth.signin.error.toomanyattempts",
+								nextattempt: nextAllowedAttempt.date,
+								});
+									} else {
+								// Only deny access
+								console.log("auth.signin.error.invalidpassword");
+								return res.status(401).json({
+								type: "auth.signin.error.invalidpassword",
+								});
+									}
+								})	            
+					} else {	
+						// Record connection & clear previous attempts
+						User.updateOne(
+							{ 
+								userid: user.userid },
+								{ "$set": { 
+									lastconnection : Date.now() ,
+									failedconnections: []
+								}
+							}
+						).then((outcome) => {
+							if (outcome.acknowledged) {
+								console.log("user.recordconnection.success");
+							} else {
+								console.log("user.recordconnection.failed");
+							}
+						})
+						.catch((error) => {
+							console.log("user.recordconnection.error");
+							console.error(error);
+						})
+						.then(() => {
+							// Grant access
+							return res.status(200).json({
+							type: "auth.signin.success",
+							data: {
+								token: jwt.sign(
+									{
+										userid: user.userid,
+										type: user.type,
+										communityid: user.communityid
+									},
+									process.env.JWT_SECRET,
+									{
+										expiresIn: "365d",
+									}
+								),
+							},
+							});
+					})
+					}
 					})
 					.catch((error) => {
-						console.log("user.recordconnection.error");
-						console.error(error);
-					})
-	              	.then(() => {
-						// Grant access
-						return res.status(200).json({
-						type: "auth.signin.success",
-						data: {
-							token: jwt.sign(
-								{
-									userid: user.userid,
-									type: user.type,
-									communityid: user.communityid
-								},
-								process.env.JWT_SECRET,
-								{
-									expiresIn: "365d",
-								}
-							),
-						},
-						});
-	              })
-		          }
-		        })
-		        .catch((error) => {
-		          console.log("auth.signin.error.onpasswordcompare", error);
-		          return res.status(500).json({
-		            type: "auth.signin.error.onpasswordcompare",
-		            error: error,
-		          });
-		        });
-	      }
-      }
+					console.log("auth.signin.error.onpasswordcompare", error);
+					return res.status(500).json({
+						type: "auth.signin.error.onpasswordcompare",
+						error: error,
+					});
+					});
+			}
+		}
+	}
     })
     .catch((error) => {
       console.log("auth.signin.error.onfind", error);
