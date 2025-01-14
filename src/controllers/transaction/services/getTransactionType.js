5c660301f6bd76f951b3a393d60709224d213dfc
pst <81512700+PireSvyt@users.noreply.github.com>
2025-01-14 11:22:40 +0000

    fixing balance

--- src/controllers/transaction/services/getTransactionType.js
+++ src/controllers/transaction/services/getTransactionType.js
@@ -1,43 +1,60 @@
 module.exports = function getTransactionType(transaction, userid) {
+  let outcome = {
+    audience: "",
+    byuser: undefined,
+    foruser: undefined,
+  };
 
-    let outcome = {
-        audience: "",
-        byuser: undefined,
-        foruser: undefined
-    }
-
+  if (transaction.type === "expense") {
     if (transaction.for.length > 1) {
-        outcome.audience = "community"
-        if (userid !== undefined) {
-            if (transaction.by === userid) {
-                outcome.byuser = true
-            }
-            if (transaction.for.includes(userid)) {
-                outcome.foruser = true
-            }
+      outcome.audience = "community";
+      if (userid !== undefined) {
+        if (transaction.by === userid) {
+          outcome.byuser = true;
         }
+        if (transaction.for.includes(userid)) {
+          outcome.foruser = true;
+        }
+      }
     } else {
       if (transaction.for.includes(transaction.by)) {
-        outcome.audience = "personal"
+        outcome.audience = "personal";
         if (userid !== undefined) {
-            if (transaction.by === userid) {
-                outcome.byuser = true
-                outcome.foruser = true
-            }
+          if (transaction.by === userid) {
+            outcome.byuser = true;
+            outcome.foruser = true;
+          }
         }
       } else {
-        outcome.audience = "transfer"
+        outcome.audience = "transfer";
         if (userid !== undefined) {
-            if (transaction.by === userid) {
-                outcome.byuser = true
-            }
-            if (transaction.for.includes(userid)) {
-                outcome.foruser = true
-            }
+          if (transaction.by === userid) {
+            outcome.byuser = true;
+          }
+          if (transaction.for.includes(userid)) {
+            outcome.foruser = true;
+          }
         }
       }
     }
-    
-    return outcome;
-    };
-  
+  } else {
+    // Revenue
+    if (transaction.for.length > 1) {
+      outcome.audience = "community";
+      if (userid !== undefined) {
+        if (transaction.for.includes(userid)) {
+          outcome.foruser = true;
+        }
+      }
+    } else {
+      outcome.audience = "personal";
+      if (userid !== undefined) {
+        if (transaction.by === userid) {
+          outcome.foruser = true;
+        }
+      }
+    }
+  }
+
+  return outcome;
+};
