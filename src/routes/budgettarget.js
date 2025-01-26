4af18afcc5429dbb271b43f05e704c3b6052d13b
pst <81512700+PireSvyt@users.noreply.github.com>
2025-01-26 18:27:10 +0000

    path

--- src/routes/budgettarget.js
+++ src/routes/budgettarget.js
@@ -3,16 +3,14 @@ const router = express.Router();
 
 const authAuthenticate = require("../controllers/auth/authAuthenticate.js");
 
-const budgettargetCreate = require("../controllers/budget/budgettargetCreate.js");
-const budgettargetSave = require("../controllers/budget/budgettargetSave.js");
-const budgettargetDelete = require("../controllers/budget/budgettargetDelete.js");
-const budgettargetGetList = require("../controllers/budget/budgettargetGetList.js");
-const budgettargetCompute = require("../controllers/budgettarget/budgettargetCompute.js");
+const budgettargetCreate = require("../controllers/budgettarget/budgettargetCreate.js");
+const budgettargetSave = require("../controllers/budgettarget/budgettargetSave.js");
+const budgettargetDelete = require("../controllers/budgettarget/budgettargetDelete.js");
+const budgettargetGetList = require("../controllers/budgettarget/budgettargetGetList.js");
 
 router.post("/v1/create", authAuthenticate, budgettargetCreate);
 router.post("/v1/save", authAuthenticate, budgettargetSave);
 router.delete("/v1/:budgettargetid", authAuthenticate, budgettargetDelete);
 router.post("/v1/getlist", authAuthenticate, budgettargetGetList);
-router.post("/v1/comute", authAuthenticate, budgettargetCompute);
 
 module.exports = router;
